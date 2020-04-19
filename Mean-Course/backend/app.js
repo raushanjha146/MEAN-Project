671e6066ee580c5b8554cc1992d668913af4cc3e
raushanjha146 <raushanjha146@gmail.com>
2020-04-19 14:10:38 +0530

    3.38: Fetching Initial Posts

--- Mean-Course/backend/app.js
+++ Mean-Course/backend/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middle layer');
-  console.log('===================');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts',(req, res, next) => {
+
+  const posts = [
+    {id: '1', title: 'First server-side posts', content: 'First post is coming from server!'},
+    {id: '2', title: 'Second server-side posts', content: 'Second post is coming from server!'},
+    {id: '3', title: 'Third server-side posts', content: 'Third post is coming from server!'}
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
