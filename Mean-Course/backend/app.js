7ef512283e0637919020641bd71c4315cdb3afd5
Raushan Kumar <raushan.kumar@barco.com>
2020-04-26 13:55:48 +0530

    4.55. Fetching Data From a Database

--- Mean-Course/backend/app.js
+++ Mean-Course/backend/app.js
@@ -45,17 +45,13 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts',(req, res, next) => {
-
-  const posts = [
-    {id: '1', title: 'First server-side posts changes', content: 'First post is coming from server!'},
-    {id: '2', title: 'Second server-side posts', content: 'Second post is coming from server!'},
-    {id: '3', title: 'Third server-side posts', content: 'Third post is coming from server!'}
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
-  });
+  Post.find()
+    .then(document => {
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: document
+      });
+    });
 });
 
 module.exports = app;
