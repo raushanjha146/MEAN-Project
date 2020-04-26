df1247b2399234c2daa09ab6b7abd3c34d90b9bf
Raushan Kumar <raushan.kumar@barco.com>
2020-04-26 14:45:00 +0530

    4.58. Updating the Frontend after Deleting Posts

--- Mean-Course/src/app/post/posts.service.ts
+++ Mean-Course/src/app/post/posts.service.ts
@@ -46,7 +46,9 @@ export class PostsService{
   deletePost(postId: string){
     this.httpClient.delete("http://localhost:3000/api/posts/" + postId)
       .subscribe(() => {
-        console.log("Deleted!");
+        const updatedPosts = this.posts.filter(post => post.id != postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
 }
